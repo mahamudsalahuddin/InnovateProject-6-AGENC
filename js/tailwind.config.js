ae9f7932b26b6f0d502b5dff56123db50e40e16d
Salahuddin Mahmud <76897399+s-mahmud52@users.noreply.github.com>
2022-09-15 12:27:33 +0600

    asenc first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary: '#6A4DF4',
+          black1: '#151515',
+          grey: '#737373',
+          successB: '#F8F6FE',
+        },
+        maxWidth: {
+            container: '1320px',
+          },
+          fontFamily: {
+            'poppins':'Poppins',
+            'inter':'Inter',
+          },
+          backgroundImage: {
+            navImage: "url('images/navImage.png')",
+          },
+      }
+    }
+  }
